3b765e2ce04fe71f9e98537f18c489a7f1b4f9d4
debasis@ukko.ag <debasis@ukko.ag>
2021-02-18 22:50:34 -0500

    added histogram

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 st.subheader('Raw data')
 st.write(data)
-st.subheader('addd subheader1')
-st.subheader('addd subheader1')
-st.subheader('addd subheader1')
-
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
